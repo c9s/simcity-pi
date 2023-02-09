a5ad8ad6f428c4ac3dff48abc0663f3d397ad58d
c9s <yoan@maicoin.com>
2023-02-09 16:42:42 +0800

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,27 @@ import { ColorModeScript } from '@chakra-ui/react'
 
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import Script from 'next/script'
+
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G1PBK2DRKW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-G1PBK2DRKW');
+          `}
+        </Script>
         <Main />
         <NextScript />
       </body>
